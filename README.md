c99855b5ab2819b1849464bc8008ca49e821e322
mofung1 <2279254178@qq.com>
2025-02-27 16:35:27 +0800

    调整描述

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # simple-tool
 
 #### 介绍
-短视频去水印小程序，uniapp
+短视频去水印小程序 golang，uniapp
+###
+短视频解析服务使用开源项目代码 https://github.com/wujunwei928/parse-video
+###
+前端使用unibest框架结合ai生成 https://www.unibest.tech
+![img.png](img.png)
+![img_1.png](img_1.png)
+
--- uniapp/pages.config.ts
+++ uniapp/pages.config.ts
@@ -3,7 +3,7 @@ import { defineUniPages } from '@uni-helper/vite-plugin-uni-pages'
 export default defineUniPages({
   globalStyle: {
     navigationStyle: 'default',
-    navigationBarTitleText: 'unibest',
+    navigationBarTitleText: 'simple-tool',
     navigationBarBackgroundColor: '#f8f8f8',
     navigationBarTextStyle: 'black',
     backgroundColor: '#FFFFFF',
